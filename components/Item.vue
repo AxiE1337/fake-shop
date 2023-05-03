cab8b2b26358b8f99dbd80470a8622fe6a455198
AxiE1337 <pixell390@gmail.com>
2023-05-03 14:09:00 +0300

    added Item component

---
+++ components/Item.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { IProduct } from '~/types'
+
+defineProps<IProps>()
+interface IProps {
+  product: IProduct
+}
+</script>
+
+<template>
+  <nuxt-img
+    :src="product.image"
+    alt="img"
+    loading="lazy"
+    class="w-1/4 rounded md:w-3/4"
+  />
+  <h1
+    @click="
+      () => {
+        navigateTo(`/item/${product.id}`)
+      }
+    "
+    class="cursor-pointer text-2xl hover:underline"
+  >
+    {{ product.title }}
+  </h1>
+  <h2>{{ 'Price ' + product.price + '$' }}</h2>
+  <h2>{{ 'Category ' + product.category }}</h2>
+  <p class="md:hidden">{{ product.description }}</p>
+  <p>{{ product.rating.rate }}</p>
+  <div class="rating">
+    <input
+      v-for="r in Math.floor(product.rating.rate)"
+      disabled
+      type="radio"
+      name="rating-1"
+      class="mask mask-star-2 bg-green-400"
+    />
+  </div>
+</template>
--- pages/category/[category].vue
+++ pages/category/[category].vue
@@ -44,34 +44,7 @@ const handleFilter = () => {
         v-for="product in handleFilter()"
         class="flex flex-col gap-2 w-4/5 p-2 shadow-md blurre-in"
       >
-        <nuxt-img
-          :src="product.image"
-          alt="img"
-          loading="lazy"
-          class="w-1/4 rounded md:w-3/4"
-        />
-        <h1
-          @click="
-            () => {
-              navigateTo(`/item/${product.id}`)
-            }
-          "
-          class="cursor-pointer text-2xl hover:underline"
-        >
-          {{ product.title }}
-        </h1>
-        <h2>{{ 'Price ' + product.price + '$' }}</h2>
-        <h2>{{ 'Category ' + product.category }}</h2>
-        <p class="md:hidden">{{ product.description }}</p>
-        <p>{{ product.rating.rate }}</p>
-        <div class="rating">
-          <input
-            v-for="r in Math.floor(product.rating.rate)"
-            type="radio"
-            name="rating-1"
-            class="mask mask-star-2 bg-green-400"
-          />
-        </div>
+        <Item :product="product" />
       </section>
     </div>
   </main>
